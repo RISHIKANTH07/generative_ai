1dd281283b39f93db5f81a6c18f7dbca66ef31ed
RISHIKANTH <rishikanth0709@gmail.com>
2025-09-28 10:39:11 +0530

    commit the dataclean

---
+++ python/text data cleaning.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import re
+import string,time
+data=pd.read_csv('IMDB Dataset.csv')
+df=pd.DataFrame(data)
+text="<h1>Welcome to<P> Python <a>Programming</h1>"
+# Convert text to lower case
+def lower_case(text):
+    data['review']=data['review'].str.lower()
+    print(data)
+# Remove Punctuation
+def removel_htmltags(text):
+    clean=re.compile('<.*?>')
+    return clean.sub(r'',text)
+print(removel_htmltags(text))
+data['review']=data['review'].apply(removel_htmltags)
+print(data)
+# Remove URLS
+def  removel_url(text):
+    url=re.compile(r'https?://\S+|www\.\S+')
+    return url.sub(r'',text)
+data['review']=data['review'].apply(removel_url)
+print(data)
+print(data['review'][10])
+#punctuation handling
+exclude=string.punctuation
+def remove_punctuation(text):
+    for char in text:
+        if char in exclude:
+          text=text=text.replace(char,"")
+    return text 
+data['review']=data['review'].apply(remove_punctuation)
+print(data)
+    
